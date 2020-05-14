7ba5a17ff7b97c06fb6f44ca00e785837b6d0655
MOHAMED TAHA <MTahaElMaghrabi@hotmail.com>
2020-05-14 17:59:49 +0300

    UI

--- SBAdmin/Startup.cs
+++ SBAdmin/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +12,7 @@ namespace SBAdmin
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
